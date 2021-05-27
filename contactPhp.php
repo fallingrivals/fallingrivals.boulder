4bc0421a9e0922f72363efaaa8c086cc8f442523
fallingrivals <46771926+fallingrivals@users.noreply.github.com>
2021-05-27 10:23:38 +0800

    Add files via upload

---
+++ contactPhp.php
@@ -0,0 +1,28 @@
+<?php
+
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $email_from = 'Boulder';
+
+    $email_subject = "Contact Us";
+
+    $email_body = "User Name: $name.\n".
+                    "User Email: $visitor_email.\n".
+                     "User Message: $message.\n";
+
+
+    $to = "escamellaedmond@gmail.com";
+
+    $headers = "From: $email_from \r\n";
+
+    $headers .= "Reply-To : $visitor_email \r\n";
+
+    mail($to,$email_subject,$email_body,$headers);
+
+    header("Location: index.html");
+
+
+
+?>
